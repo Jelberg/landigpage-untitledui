27176beb7131897934a19937dc0d213f1fb9608d
Jessica Elberg <thejessidev@gmail.com>
2024-02-20 23:40:27 -0400

    set styles components

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,10 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors:{
+      "purple-ui": "#53389e",
+      "lightpurple-ui": "#7f56d9",
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
