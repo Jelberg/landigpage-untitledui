752283d485f38c434261eb85279877158677038d
Jessica Elberg <thejessidev@gmail.com>
2024-02-21 19:57:55 -0400

    feat: header and footer

---
+++ src/app/components/layouts/Header.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image"
+
+export default function Header() {
+    return <div className="grid grid-cols-3 place-content-between bg-white w-full">
+        <div className="flex items-center">
+        <Image src="/logo/untitledui.png" alt="untitlrd-ui" width={0} height={0} sizes="100vw" style={{height:"60px", width:"auto"}}/>
+        <h3 className="inline-block align-middle">Untitled UI</h3>
+        </div>
+        
+        <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
+            <ul className="flex">
+                <li><a className="text-black" href="#">Home</a></li>
+                <li><a className="text-black" href="#">Products</a></li>
+                <li><a className="text-black" href="#">Resource</a></li>
+                <li><a className="text-black" href="#">Pricing</a></li>
+            </ul>
+        </nav>
+
+        <div className="flex justify-end gap-5">
+            <button className="text-gray-500 bg-transparent">Log in</button>
+            <button className="my-5">Sign up</button>
+        </div>
+  </div>
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,11 +14,14 @@ import Avatar from "./components/avatar/Avatar";
 import IconWithText from "./components/inconWithText/IconWithText";
 import QuestionContent from "./components/question/content/QuestionContent";
 import Collage from "./components/collage/Collage";
+import Header from "./components/layouts/Header";
+import FooterUI from "./components/layouts/FooterUI";
 
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+          <Header />
       <h1 className="text-purple-ui">H1 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure voluptas eius corrupti. Rerum laborum, corrupti at et non recusandae architecto nostrum, suscipit harum veritatis dolores aut enim commodi fugit soluta!</h1>
       <h2 className="text-lightpurple-ui">H2 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquid expedita est quaerat voluptate, cupiditate consequuntur illo repellat perferendis maiores neque consectetur? Amet quibusdam, non error aliquam incidunt molestias libero beatae.</h2>
       <h3>H3 Lorem ipsum dolor sit amet con </h3>
@@ -42,6 +45,7 @@ export default function Home() {
     <IconWithText type="img" width={100} height={100}/>
     <QuestionContent quetion="Is there a free trial available?" summary="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum debitis eaque vitae deserunt labore consequuntur veritatis incidunt blanditiis sunt magnam, vel distinctio quibusdam officiis ipsa, neque sint laborum eos optio." />
     <Collage />
+    <FooterUI />
     </main>
   );
 }
