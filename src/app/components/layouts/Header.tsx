3daf4c79d6690ad000e55ebc5990a926672b5bbc
Jessica Elberg <thejessidev@gmail.com>
2024-02-22 01:14:04 -0400

    change: header

--- src/app/components/layouts/Header.tsx
+++ src/app/components/layouts/Header.tsx
@@ -1,13 +1,12 @@
 import Image from "next/image"
 
 export default function Header() {
-    return <div className="grid grid-cols-3 place-content-evenly bg-white w-full">
-        <div className="flex items-center">
+    return <div className="grid grid-cols-9 place-content-evenly bg-white w-full">
+        <div className="flex items-center col-span-4 ">
         <Image src="/logo/untitledui.png" alt="untitlrd-ui" width={0} height={0} sizes="100vw" style={{height:"60px", width:"auto"}}/>
         <h3 className="inline-block align-middle">Untitled UI</h3>
-        </div>
-        
-        <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
+
+        <nav className="flex items-center mx-auto" aria-label="Global">
             <ul className="flex">
                 <li><a className="text-black" href="#">Home</a></li>
                 <li><a className="text-black" href="#">Products</a></li>
@@ -16,7 +15,11 @@ export default function Header() {
             </ul>
         </nav>
 
-        <div className="flex justify-end gap-5">
+        </div>
+        
+        
+
+        <div className="flex justify-end gap-5 col-start-8 col-span-2">
             <button className="text-gray-500 bg-transparent">Log in</button>
             <button className="my-5">Sign up</button>
         </div>
