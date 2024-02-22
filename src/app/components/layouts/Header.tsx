582c124ff45d964a0c563af06b4b85cc1defbde2
Jessica Elberg <thejessidev@gmail.com>
2024-02-22 05:38:19 -0400

    change: font size title

--- src/app/components/layouts/Header.tsx
+++ src/app/components/layouts/Header.tsx
@@ -5,7 +5,7 @@ export default function Header() {
     return <div className="grid items-center grid-cols-4 sm:grid-cols-4 md:grid-cols-9 place-content-between bg-white w-full">
         <div className="flex items-center col-span-2 sm:col-span-2 md:col-span-4 ">
         <Image src="/logo/untitledui.png" alt="untitlrd-ui" width={0} height={0} sizes="100vw" style={{height:"60px", width:"auto"}}/>
-        <h3 className="inline-block align-middle text-xl sm:text-2xl">Untitled UI</h3>
+        <h3 className="inline-block align-middle text-xl sm:text-lg">Untitled UI</h3>
 
         <nav className="hidden sm:hidden md:flex items-center mx-auto " aria-label="Global">
             <ul className="flex">
